8dd12ad8a38faf561888b40ee00b955bccc47b6e
Komola Khidirova <komolakhidirova@gmail.com>
2025-05-12 16:40:57 +0500

    stats card

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -7,7 +7,7 @@ const AdminLayout = () => {
 		<div className='admin-layout'>
 			<MobileSidebar />
 
-			<aside className='w-full max-w-{270px} hidden lg:block'>
+			<aside className='w-full max-w-[270px] hidden lg:block'>
 				<SidebarComponent width={270} enableGestures={false}>
 					<NavItems />
 				</SidebarComponent>
